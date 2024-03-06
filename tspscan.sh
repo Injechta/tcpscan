4c43cd49c2c2ec9c4c3b3a512b89f50e673fb777
Gregory ELBAJOURY <34866703+Injechta@users.noreply.github.com>
2024-03-06 10:27:18 +0100

    Update tspscan.sh

--- tspscan.sh
+++ tspscan.sh
@@ -7,7 +7,7 @@ function  tcpscan ()
         ip=$1
         for port in $(seq 1 30);
         do
-                timeout 1 bash -c "</dev/tcp/$ip/$port && echo Port $port is open || echo Port $port is closed > /dev/null" 2>/dev/null || echo Connection timeout > /dev/null
+                timeout 1 bash -c "</dev/tcp/$ip/$port && echo Le Port $port est ouvert || echo Le Port $port est fermé > /dev/null" 2>/dev/null || echo Connection timeout > /dev/null
 done
 
 }
